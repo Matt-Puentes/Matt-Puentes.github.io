c3465b1abd0b21356be5fda509aa202855a9774e
Matthew Puentes <matt.puentes@gmail.com>
2018-12-05 17:16:28 -0500

    Added pet

---
+++ HCI-p6/Dogscript.js
@@ -0,0 +1,62 @@
+console.log("Game begin!")
+nextTick = -1
+
+var sceneEl = document.querySelector('a-scene');
+var dog = sceneEl.querySelector('#dog');
+
+
+addBounceAnimation(dog)
+
+function addBounceAnimation(entity) {
+    var dogScale = dog.getAttribute('scale');
+    var dogScale2 = Object.assign({}, dogScale);
+    dogScale2.y = dogScale2.y + 0.5
+    dogScale2.x = dogScale2.x + 0.10
+    dogScale2.z = dogScale2.z + 0.10
+    var dogPos = dog.getAttribute('position');
+    var dogPos2 = Object.assign({}, dogPos);
+    dogPos2.y = dogPos2.y + 0.25
+    var len = 500
+
+    entity.setAttribute('animation__scale1',{
+        property:'scale',
+        from: vec3tostr(dogScale),
+        to: vec3tostr(dogScale2),
+        startEvents: 'bounce',
+        dur: len
+    })
+    entity.setAttribute('animation__pos1',{
+        property:'position',
+        from: vec3tostr(dogPos),
+        to: vec3tostr(dogPos2),
+        startEvents: 'bounce',
+        dur: len
+    })
+    entity.setAttribute('animation__scale2',{
+        property:'scale',
+        from: vec3tostr(dogScale2),
+        to: vec3tostr(dogScale),
+        startEvents: 'bounce',
+        delay: len,
+        dur: len
+    })
+    entity.setAttribute('animation__pos2',{
+        property:'position',
+        from: vec3tostr(dogPos2),
+        to: vec3tostr(dogPos),
+        startEvents: 'bounce',
+        delay: len,
+        dur: len
+    })
+}
+
+function vec3tostr(vec3){
+    return vec3.x + " " + vec3.y + " " + vec3.z
+}
+
+window.addEventListener("keydown", function (event) {
+	if(event.key == "r"){
+        console.log("jjkm")
+        dog.emit('bounce')
+    }
+})
--- MatchThree.js
+++ MatchThree.js
@@ -2,12 +2,14 @@ var canvas;
 var ctx;
 var boardHeight;
 var boardWidth;
+var colorNum;
+var nextTick;
+var selectHorizontal = false
+var selectPos = [0,0]
 var boxSize = 40;
-var firstFruit = new Point();
 function main(){
 	canvas = document.getElementById('MatchThreeCanvas')
 	ctx = canvas.getContext('2d')
-    canvas.addEventListener("click", onClick, false)
 	document.getElementById("reset").onclick = resetGame;
 	resetGame();
 	tick();
@@ -21,6 +23,7 @@ function resetGame(){
 function checkInput(){
 	boardHeight = parseInt(document.getElementById('height-box').value)
 	boardWidth = parseInt(document.getElementById('width-box').value)
+	colorNum = parseInt(document.getElementById('colors-box').value)
 
 	if(boardHeight < 4){
 		boardHeight = 4;
@@ -35,42 +38,131 @@ function checkInput(){
 	canvas.height = boxSize * boardHeight;
 }
 
+function generateBoard(){
+	var newBoard = []
+	for(var x = 0; x < boardWidth; x++){
+		newBoard[x] = []
+		for(var y = 0; y < boardHeight; y++){
+			if(y < boardHeight/2)
+				newBoard[x][y] = 0
+			else
+				newBoard[x][y] = Math.floor((Math.random() * 5)+ 1)
+		}
+	}
+	return newBoard;
+}
+
 function initialize(){
+	board = generateBoard()
+
 }
 
 function tick(){
+	drawBoard()
+
+	//check for matches
+	
+
+}
+
+function swapSelectedBlocks(){
+	block1 = board[selectPos[0]][selectPos[1]]
+	if(selectHorizontal)
+		block2 = board[selectPos[0]][selectPos[1] + 1]
+	else
+		block2 = board[selectPos[0] + 1][selectPos[1]]
+
+	if(block1 == 0 || block2 == 0)
+		return;
+	else if(block1 == block2)
+		return;
+
+	else {
+		board[selectPos[0]][selectPos[1]] = block2
+		if(selectHorizontal)
+			board[selectPos[0]][selectPos[1] + 1] = block1
+		else
+			board[selectPos[0] + 1][selectPos[1]] = block1
+
+	}
 }
 
 function drawBoard(){
-	//"type" should be "head", "snake" "empty", "food"
-	if(type == "head"){
-		ctx.lineWidth = boxSize/40;
-		ctx.fillStyle = 'rgb(100,0,0)';
-	}
-	if(type == "snake"){
-		ctx.lineWidth = boxSize/40;
-		ctx.fillStyle = 'rgb(0,100,0)';
-	}
-	if(type == "empty" || type == "food"){
-		ctx.lineWidth = boxSize/40;
-		ctx.fillStyle = 'rgb(100,100,200)';
+	ctx.strokeStyle = 'rgb(0,0,0)';
+	for(var x = 0; x < boardWidth; x++){
+		for(var y = 0; y < boardHeight; y++){
+			switch (board[x][y]) {
+				case 1:
+					ctx.fillStyle = 'rgb(100,0,0)';
+				break;
+				case 2:
+					ctx.fillStyle = 'rgb(0,0,100)';
+				break;
+				case 3:
+					ctx.fillStyle = 'rgb(0,100,0)';
+				break;
+				case 4:
+					ctx.fillStyle = 'rgb(100,100,0)';
+				break;
+				case 5:
+					ctx.fillStyle = 'rgb(0,100,100)';
+				break;
+				default:
+					ctx.fillStyle = 'rgb(25,20,25)';
+
+			}
+			ctx.fillRect(x * boxSize, y * boxSize, boxSize, boxSize)
+			ctx.lineWidth = boxSize/40;
+			ctx.strokeRect(x * boxSize, y * boxSize, boxSize, boxSize)
+		}
 	}
 
-	ctx.fillRect(x * boxSize, y * boxSize, boxSize, boxSize);
-	ctx.strokeRect(x * boxSize, y * boxSize, boxSize, boxSize);
-
-	if(type == "food"){
-		ctx.fillStyle = 'rgb(0,0,255)'
-		ctx.beginPath();
-		ctx.arc((x * boxSize) + boxSize/2,
-			(y * boxSize) + boxSize/2,
-			(.5*boxSize)/2, 0, 2 * Math.PI, false);
-		ctx.fill();
-	}
+	ctx.strokeStyle = 'rgb(255,255,255)';
+	if(selectHorizontal)
+		ctx.strokeRect(selectPos[0] * boxSize, selectPos[1] * boxSize, boxSize, boxSize*2)
+	else
+		ctx.strokeRect(selectPos[0] * boxSize, selectPos[1] * boxSize, boxSize*2, boxSize)
 }
 
-function onClick(){
-    var rect = canvas.getBoundingClientRect();
-    boardPos[0] = Math.floor((evt.clientX - rect.left) / boxSize)
-    boardPos[1] = Math.floor((evt.clientY - rect.top) / boxSize)
-}
+window.addEventListener("keydown", function (event) {
+	var upkeys= ["ArrowUp", "j", "J"];
+	var downkeys = ["ArrowDown", "k", "K"];
+	var leftkeys = ["ArrowLeft", "h", "H"];
+	var rightkeys = ["ArrowRight", "l", "L"];
+	var flipkeys = [";", "Enter", "Tab"];
+	var swapkeys = [" ", ".", ","];
+	if(upkeys.indexOf(event.key) >= 0){
+		selectPos[1]--;
+	}
+	if(downkeys.indexOf(event.key) >= 0){
+		selectPos[1]++;
+	}
+	if(leftkeys.indexOf(event.key) >= 0){
+		selectPos[0]--;
+	}
+	if(rightkeys.indexOf(event.key) >= 0){
+		selectPos[0]++;
+	}
+	if(flipkeys.indexOf(event.key) >= 0){
+		selectHorizontal = !selectHorizontal;
+	}
+	if(swapkeys.indexOf(event.key) >= 0){
+		swapSelectedBlocks();
+		tick();
+	}
+
+
+	if(event.key == "r" || event.key == "R")
+		resetGame()
+
+	if(selectHorizontal){
+		selectPos[0] = Math.min(Math.max(selectPos[0] ,0), boardWidth - 1)
+		selectPos[1] = Math.min(Math.max(selectPos[1] ,0), boardHeight - 2)
+	}
+	else{
+		selectPos[0] = Math.min(Math.max(selectPos[0] ,0), boardWidth - 2)
+		selectPos[1] = Math.min(Math.max(selectPos[1] ,0), boardHeight - 1)
+	}
+
+	drawBoard()
+}, true);

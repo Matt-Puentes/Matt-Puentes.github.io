7464cfe00d72a796b0c7d84ee94ff54636b315e2
Matthew Puentes <matt.puentes@gmail.com>
2018-12-06 18:02:57 -0500

    Added fixed a-pet

--- HCI-p6/Dogscript.js
+++ HCI-p6/Dogscript.js
@@ -1,18 +1,36 @@
+var sceneEl;
+var dog;
+var dogWrapper;
 function main(){
     console.log("Game begin!")
-    nextTick = -1
-    var sceneEl = document.querySelector('a-scene');
-    var dog = sceneEl.querySelector('#dog');
+    sceneEl = document.querySelector('a-scene');
+    dog = sceneEl.querySelector('#dog');
+    dogWrapper = sceneEl.querySelector('#dog-wrapper');
 
     loadDog()
     addBounceAnimation(dog)
     addJumpAnimation(dog)
+    addSadAnimation(dog)
 
     dog.addEventListener('click', function() {
-        console.log("bounce")
         dog.emit('bounce');
+        dog.emit('jump');
     });
+
+    dogwalk()
 }
+
+var nextTick = -1
+function dogwalk(){
+    if(Math.random() > 0.5){
+        addRandomWalk(dogWrapper)
+        dog.emit('walk')
+    }
+    if(nextTick != -1)
+        clearTimeout(nextTick)
+    nextTick = setTimeout(dogwalk, 2000);
+}
+
 
 function loadDog(){
     dog.setAttribute('scale', {x: .5, y: .5, z: .5}, true);
@@ -22,8 +40,8 @@ function addBounceAnimation(entity) {
     var dogScale = entity.getAttribute('scale');
     var dogScale2 = Object.assign({}, dogScale);
     dogScale2.y = dogScale2.y * 1.5
-    dogScale2.x = dogScale2.x * 1.10
-    dogScale2.z = dogScale2.z * 1.10
+    dogScale2.x = dogScale2.x * 0.8
+    dogScale2.z = dogScale2.z * 0.8
     var dogPos = entity.getAttribute('position');
     var dogPos2 = Object.assign({}, dogPos);
     dogPos2.y = dogPos2.y + 0.2//(entity.getAttribute('geometry').height * 0.5)
@@ -66,6 +84,74 @@ function addBounceAnimation(entity) {
     })
 }
 
+function addSadAnimation(entity) {
+    var dogPos = entity.getAttribute('position')
+    var dogPos2 = Object.assign({}, dogPos);
+    var dogRot = entity.getAttribute('rotation')
+    var dogRot2 = Object.assign({}, dogRot);
+    var len = 3000
+
+    dogPos2.y -= 0.05
+    dogRot2.z += -15
+
+    var dogScale = entity.getAttribute('scale');
+    var dogScale2 = Object.assign({}, dogScale);
+    dogScale2.y = dogScale2.y * 0.8
+    dogScale2.x = dogScale2.x * 1.2
+    dogScale2.z = dogScale2.z * 1.2
+
+    entity.setAttribute('animation__pos1_s',{
+        property:'position',
+        from: vec3tostr(dogPos),
+        to: vec3tostr(dogPos2),
+        startEvents: 'sad',
+        dur: len
+    })
+
+    entity.setAttribute('animation__pos2_s',{
+        property:'position',
+        from: vec3tostr(dogPos2),
+        to: vec3tostr(dogPos),
+        startEvents: 'sad',
+        delay: len,
+        dur: len
+    })
+
+    entity.setAttribute('animation__rot1_s', {
+        property: 'rotation',
+        from: vec3tostr(dogRot),
+        to: vec3tostr(dogRot2),
+        startEvents: 'sad',
+        dur: len
+    })
+
+    entity.setAttribute('animation__rot2_s', {
+        property: 'rotation',
+        from: vec3tostr(dogRot2),
+        to: vec3tostr(dogRot),
+        startEvents: 'sad',
+        delay: len * 2,
+        dur: len
+    })
+
+    entity.setAttribute('animation__scale1_s',{
+        property:'scale',
+        from: vec3tostr(dogScale),
+        to: vec3tostr(dogScale2),
+        startEvents: 'sad',
+        dur: len
+    })
+
+    entity.setAttribute('animation__scale2_s',{
+        property:'scale',
+        from: vec3tostr(dogScale2),
+        to: vec3tostr(dogScale),
+        startEvents: 'sad',
+        delay: len * 2,
+        dur: len
+    })
+}
+
 function addJumpAnimation(entity) {
     var dogPos = entity.getAttribute('position')
     var dogPos2 = Object.assign({}, dogPos);
@@ -74,9 +160,9 @@ function addJumpAnimation(entity) {
     var len = 140
 
     dogPos2.y += 1
-    dogRot2.x += -40
+    dogRot2.z += 20
 
-    entity.setAttribute('animation__pos1',{
+    entity.setAttribute('animation__pos1_j',{
         property:'position',
         from: vec3tostr(dogPos),
         to: vec3tostr(dogPos2),
@@ -84,7 +170,7 @@ function addJumpAnimation(entity) {
         dur: len
     })
 
-    entity.setAttribute('animation__pos2',{
+    entity.setAttribute('animation__pos2_j',{
         property:'position',
         from: vec3tostr(dogPos2),
         to: vec3tostr(dogPos),
@@ -93,7 +179,7 @@ function addJumpAnimation(entity) {
         dur: len
     })
 
-    entity.setAttribute('animation__rot1', {
+    entity.setAttribute('animation__rot1_j', {
         property: 'rotation',
         from: vec3tostr(dogRot),
         to: vec3tostr(dogRot2),
@@ -101,7 +187,7 @@ function addJumpAnimation(entity) {
         dur: len
     })
 
-    entity.setAttribute('animation__rot2', {
+    entity.setAttribute('animation__rot2_j', {
         property: 'rotation',
         from: vec3tostr(dogRot2),
         to: vec3tostr(dogRot),
@@ -117,26 +203,35 @@ function addRandomWalk(entity) {
 
 
     len = 1000;
-    dogPos2.x += Math.random() * 2 - 1
-    dogPos2.z += Math.random() * 2 - 1
+    var dx = (Math.random() * 4) - 2
+    var dz = (Math.random() * 4) - 2
+    dogPos2.x += dx
+    dogPos2.z += dz
 
+    var dogRot = entity.getAttribute('position')
+    var dogRot2 = Object.assign({}, dogPos);
+
+    var rotation = Math.atan2(dz,dx)
+    console.log(rotation)
+    dogRot2.y = dogRot2.y - (rotation * 180 / Math.PI)
+
+    dogWrapper.setAttribute('animation__rot1',{
+        property:'rotation',
+        from: vec3tostr(dogRot),
+        to: vec3tostr(dogRot2),
+        startEvents: 'walk',
+        dur: 300
+    })
 
     entity.setAttribute('animation__pos1',{
         property:'position',
         from: vec3tostr(dogPos),
         to: vec3tostr(dogPos2),
         startEvents: 'walk',
+        delay: 300,
         dur: len
     })
 
-    entity.setAttribute('animation__pos2',{
-        property:'position',
-        from: vec3tostr(dogPos2),
-        to: vec3tostr(dogPos),
-        startEvents: 'walk',
-        delay: len,
-        dur: len
-    })
 }
 
 function vec3tostr(vec3){
@@ -154,9 +249,9 @@ window.addEventListener("keydown", function (event) {
         dog.emit('jump')
     }
 
-	if(event.key == "t"){
-        addRandomWalk(dog)
-        console.log("walk")
-        dog.emit('walk')
+	if(event.key == "l"){
+        //addRandomWalk(dog)
+        console.log("sad")
+        dog.emit('sad')
     }
 })

aea3753d5049ed500210fbf7efbb347fabcc6afe
Matthew Puentes <matt.puentes@gmail.com>
2018-12-06 13:38:31 -0500

    Updated pet module to be up-to-date

--- HCI-p6/Dogscript.js
+++ HCI-p6/Dogscript.js
@@ -1,22 +1,38 @@
-console.log("Game begin!")
-nextTick = -1
+function main(){
+    console.log("Game begin!")
+    nextTick = -1
+    var sceneEl = document.querySelector('a-scene');
+    var dog = sceneEl.querySelector('#dog');
 
-var sceneEl = document.querySelector('a-scene');
-var dog = sceneEl.querySelector('#dog');
+    loadDog()
+    addBounceAnimation(dog)
+    addJumpAnimation(dog)
 
+    dog.addEventListener('click', function() {
+        console.log("bounce")
+        dog.emit('bounce');
+    });
+}
 
-addBounceAnimation(dog)
+function loadDog(){
+    dog.setAttribute('scale', {x: .5, y: .5, z: .5}, true);
+}
 
 function addBounceAnimation(entity) {
-    var dogScale = dog.getAttribute('scale');
+    var dogScale = entity.getAttribute('scale');
     var dogScale2 = Object.assign({}, dogScale);
-    dogScale2.y = dogScale2.y + 0.5
-    dogScale2.x = dogScale2.x + 0.10
-    dogScale2.z = dogScale2.z + 0.10
-    var dogPos = dog.getAttribute('position');
+    dogScale2.y = dogScale2.y * 1.5
+    dogScale2.x = dogScale2.x * 1.10
+    dogScale2.z = dogScale2.z * 1.10
+    var dogPos = entity.getAttribute('position');
     var dogPos2 = Object.assign({}, dogPos);
-    dogPos2.y = dogPos2.y + 0.25
+    dogPos2.y = dogPos2.y + 0.2//(entity.getAttribute('geometry').height * 0.5)
     var len = 500
+
+    console.log(dogScale)
+    console.log(dogScale2)
+    console.log(dogPos)
+    console.log(dogPos2)
 
     entity.setAttribute('animation__scale1',{
         property:'scale',
@@ -50,12 +66,97 @@ function addBounceAnimation(entity) {
     })
 }
 
+function addJumpAnimation(entity) {
+    var dogPos = entity.getAttribute('position')
+    var dogPos2 = Object.assign({}, dogPos);
+    var dogRot = entity.getAttribute('rotation')
+    var dogRot2 = Object.assign({}, dogRot);
+    var len = 140
+
+    dogPos2.y += 1
+    dogRot2.x += -40
+
+    entity.setAttribute('animation__pos1',{
+        property:'position',
+        from: vec3tostr(dogPos),
+        to: vec3tostr(dogPos2),
+        startEvents: 'jump',
+        dur: len
+    })
+
+    entity.setAttribute('animation__pos2',{
+        property:'position',
+        from: vec3tostr(dogPos2),
+        to: vec3tostr(dogPos),
+        startEvents: 'jump',
+        delay: len,
+        dur: len
+    })
+
+    entity.setAttribute('animation__rot1', {
+        property: 'rotation',
+        from: vec3tostr(dogRot),
+        to: vec3tostr(dogRot2),
+        startEvents: 'jump',
+        dur: len
+    })
+
+    entity.setAttribute('animation__rot2', {
+        property: 'rotation',
+        from: vec3tostr(dogRot2),
+        to: vec3tostr(dogRot),
+        startEvents: 'jump',
+        delay: len,
+        dur: len
+    })
+}
+
+function addRandomWalk(entity) {
+    var dogPos = entity.getAttribute('position')
+    var dogPos2 = Object.assign({}, dogPos);
+
+
+    len = 1000;
+    dogPos2.x += Math.random() * 2 - 1
+    dogPos2.z += Math.random() * 2 - 1
+
+
+    entity.setAttribute('animation__pos1',{
+        property:'position',
+        from: vec3tostr(dogPos),
+        to: vec3tostr(dogPos2),
+        startEvents: 'walk',
+        dur: len
+    })
+
+    entity.setAttribute('animation__pos2',{
+        property:'position',
+        from: vec3tostr(dogPos2),
+        to: vec3tostr(dogPos),
+        startEvents: 'walk',
+        delay: len,
+        dur: len
+    })
+}
+
 function vec3tostr(vec3){
     return vec3.x + " " + vec3.y + " " + vec3.z
 }
 
 window.addEventListener("keydown", function (event) {
 	if(event.key == "r"){
+        console.log("bounce")
         dog.emit('bounce')
+    }
+
+	if(event.key == "j"){
+        console.log("jump")
+        dog.emit('jump')
+    }
+
+	if(event.key == "t"){
+        addRandomWalk(dog)
+        console.log("walk")
+        dog.emit('walk')
     }
 })
